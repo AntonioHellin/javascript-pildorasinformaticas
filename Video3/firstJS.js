e99333396ecf810d5a0431e9644a19ae82c8c13b
Antonio Hellin <antonio.hellinh@gmail.com>
2021-01-26 13:23:36 +0100

    Adding video3, video6 and video7 folders.

---
+++ Video3/firstJS.js
@@ -0,0 +1,14 @@
+/*
+
+Declaración de variables:
+
+var puntuacion;
+puntuacion = 0;
+
+o var puntuacion = 0;
+
+var puntuacion = 0, record = 100, jugador = "Piti";
+
+*/
+
+alert("HOLA ALUMNOSSSSSSSS!");
